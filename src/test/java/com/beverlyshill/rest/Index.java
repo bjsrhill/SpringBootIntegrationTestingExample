5fd1c14ba0cb9b2e8284eda748d101d8235ed921
Beverly S Hill <bjsrhill@gmail.com>
2019-03-13 12:21:28 -0400

    Added Javadoc

--- src/test/java/com/beverlyshill/rest/Index.java
+++ src/test/java/com/beverlyshill/rest/Index.java
@@ -6,10 +6,23 @@ import org.apache.commons.lang3.StringUtils;
 
 import io.restassured.response.Response;
 
+/**
+ * Index page object class
+ * 
+ * @author beverlyshill
+ *
+ */
 public class Index {
 	
 	private String [] listOfStrings = {"Beverly S. Hill", "Professional Experience", "Other Experience", "Technical Skills", "Training", "Education"};
-		
+	
+	/**
+	 * Makes a REST call to get the menus from 
+	 * the Index page of the application being tested
+	 * 
+	 * @return an ArrayList of Strings representing 
+	 * the Index page menus
+	 */
 	public ArrayList<String> getIndexMenus() {
 		ArrayList<String> foundIndexMenus = new ArrayList<String>();
 		Response indexMenusResponse = beverlyshillREST.getIndexMenusResponse();
@@ -18,6 +31,13 @@ public class Index {
 		return foundIndexMenus;
 	}
 
+	/**
+	 * Gets the expected menu items for the Index page
+	 * of the application being tested
+	 * 
+	 * @return an ArrayList of Strings representing
+	 * the expected Index page menu items
+	 */
 	public ArrayList<String> getExpectedMenus() {
 		String output = "\"" + StringUtils.join(listOfStrings , "\",\"") + "\"";
 		ArrayList<String> expected = new ArrayList<String>();
--- src/test/java/com/beverlyshill/rest/beverlyshillREST.java
+++ src/test/java/com/beverlyshill/rest/beverlyshillREST.java
@@ -9,11 +9,22 @@ import io.restassured.RestAssured;
 import io.restassured.parsing.Parser;
 import io.restassured.response.Response;
 
+/**
+ * 
+ * Handles REST calls
+ * @author beverlyshill
+ *
+ */
 public class beverlyshillREST {
 	
 	private static Response getIndexMenusResponse;
 	private static ArrayList<List> indexMenus;
 
+	/**
+	 * Gets the REST menu data from
+	 * the Index page of the application
+	 * begin tested
+	 */
 	public static void getIndexMenus() {
 		RestAssured.baseURI = "http://beverlyshill.solutions/";
 		RestAssured.basePath = "index";
@@ -28,6 +39,13 @@ public class beverlyshillREST {
                 extract().response();
 	}
 	
+	/**
+	 * Gets the response from the REST call
+	 * made in getIndexMenus
+	 * 
+	 * @return a Response object representing
+	 * the Index page menus
+	 */
 	public static Response getIndexMenusResponse() {
 		return getIndexMenusResponse;
 	}
